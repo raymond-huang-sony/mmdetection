d492020a056a3f2654973ecf8802bc3efba89f1c
raymond-huang-sony <raymond.huang@sony.com>
2024-07-31 17:55:58 +0200

    remove dino_head

--- mmdet/models/layers/transformer/dinov2_layers/__init__.py
+++ mmdet/models/layers/transformer/dinov2_layers/__init__.py
@@ -1,6 +1,5 @@
 from .attention import MemEffAttention
 from .block import NestedTensorBlock
-from .dino_head import DINOHead
 from .mlp import Mlp
 from .patch_embed import PatchEmbed
 from .swiglu_ffn import SwiGLUFFN, SwiGLUFFNFused
