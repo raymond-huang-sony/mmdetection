8ed775d921457f49bf6d1877c26faf07241e3d73
raymond-huang-sony <raymond.huang@sony.com>
2024-10-23 20:15:18 +0200

    update adaptor_modules.py

--- mmdet/models/layers/transformer/adaptor_modules.py
+++ mmdet/models/layers/transformer/adaptor_modules.py
@@ -1,17 +1,15 @@
-import logging
 from functools import partial
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint as cp
+from mmcv.cnn import build_norm_layer
+from ..ffn import ConvFFN
+from ..ms_deform_attn import MSDeformAttn
 from timm.models.layers import DropPath
 
-from ..ffn import ConvFFN
-from mmcv.cnn import build_norm_layer
-from ..ms_deform_attn import MSDeformAttn
-
-_logger = logging.getLogger(__name__)
+from dinov2_layers.attention import MemEffCrossAttention
 
 
 def get_reference_points(spatial_shapes, device, dtype):
@@ -88,26 +86,49 @@ class Extractor(nn.Module):
         drop_path=0.0,
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
         with_cp=False,
+        moe_cfg=None,
+        adapter_use_deform_attn=True,
     ):
         super().__init__()
         self.query_norm = norm_layer(dim)
         self.feat_norm = norm_layer(dim)
-        self.attn = MSDeformAttn(
-            d_model=dim,
-            n_levels=n_levels,
-            n_heads=num_heads,
-            n_points=n_points,
-            ratio=deform_ratio,
-        )
+        self.adapter_use_deform_attn = adapter_use_deform_attn
+        if self.adapter_use_deform_attn:
+            self.attn = MSDeformAttn(
+                d_model=dim,
+                n_levels=n_levels,
+                n_heads=num_heads,
+                n_points=n_points,
+                ratio=deform_ratio,
+                moe_cfg=moe_cfg,
+                attn_moe=(moe_cfg and moe_cfg.extractor_attn_moe),
+            )
+        else:
+            self.attn = MemEffCrossAttention(dim)
         self.with_cffn = with_cffn
         self.with_cp = with_cp
 
-        if with_cffn:
-            self.ffn = ConvFFN(
-                in_features=dim,
-                hidden_features=int(dim * cffn_ratio),
-                drop=drop,
-            )
+        self.moe_cfg = moe_cfg
+        if (moe_cfg and moe_cfg.ffn_moe) or with_cffn:
+            if moe_cfg and moe_cfg.ffn_moe:
+                print('Use MoE FFN')
+                hidden_features = dim
+                if moe_cfg.use_conv_ffn:
+                    hidden_features = int(dim * cffn_ratio)
+
+                self.ffn = MoEFFN(
+                    in_features=dim,
+                    hidden_features=hidden_features,
+                    drop=drop,
+                    moe_cfg=moe_cfg,
+                )
+            else:
+                self.ffn = ConvFFN(
+                    in_features=dim,
+                    hidden_features=int(dim * cffn_ratio),
+                    drop=drop,
+                )
+
             self.ffn_norm = norm_layer(dim)
             self.drop_path = (
                 DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
@@ -126,18 +147,21 @@ class Extractor(nn.Module):
     ):
         def _inner_forward(query, feat):
 
-            attn = self.attn(
-                self.query_norm(query),
-                reference_points,
-                self.feat_norm(feat),
-                spatial_shapes,
-                level_start_index,
-                None,
-                **kwargs,
-            )
+            if self.adapter_use_deform_attn:
+                attn = self.attn(
+                    self.query_norm(query),
+                    reference_points,
+                    self.feat_norm(feat),
+                    spatial_shapes,
+                    level_start_index,
+                    None,
+                    **kwargs,
+                )
+            else:
+                attn = self.attn(self.query_norm(query), self.feat_norm(feat))
             query = query + attn
 
-            if self.with_cffn:
+            if (self.moe_cfg and self.moe_cfg.ffn_moe) or self.with_cffn:
                 query = query + self.drop_path(
                     self.ffn(self.ffn_norm(query), H, W, **kwargs)
                 )
@@ -162,18 +186,26 @@ class Injector(nn.Module):
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
         init_values=0.0,
         with_cp=False,
+        moe_cfg=None,
+        adapter_use_deform_attn=True,
     ):
         super().__init__()
         self.with_cp = with_cp
         self.query_norm = norm_layer(dim)
         self.feat_norm = norm_layer(dim)
-        self.attn = MSDeformAttn(
-            d_model=dim,
-            n_levels=n_levels,
-            n_heads=num_heads,
-            n_points=n_points,
-            ratio=deform_ratio,
-        )
+        self.adapter_use_deform_attn = adapter_use_deform_attn
+        if self.adapter_use_deform_attn:
+            self.attn = MSDeformAttn(
+                d_model=dim,
+                n_levels=n_levels,
+                n_heads=num_heads,
+                n_points=n_points,
+                ratio=deform_ratio,
+                moe_cfg=moe_cfg,
+                attn_moe=(moe_cfg and moe_cfg.injector_attn_moe),
+            )
+        else:
+            self.attn = MemEffCrossAttention(dim)
         self.gamma = nn.Parameter(
             init_values * torch.ones(dim), requires_grad=True
         )
@@ -189,15 +221,18 @@ class Injector(nn.Module):
     ):
         def _inner_forward(query, feat):
 
-            attn = self.attn(
-                self.query_norm(query),
-                reference_points,
-                self.feat_norm(feat),
-                spatial_shapes,
-                level_start_index,
-                None,
-                **kwargs,
-            )
+            if self.adapter_use_deform_attn:
+                attn = self.attn(
+                    self.query_norm(query),
+                    reference_points,
+                    self.feat_norm(feat),
+                    spatial_shapes,
+                    level_start_index,
+                    None,
+                    **kwargs,
+                )
+            else:
+                attn = self.attn(self.query_norm(query), self.feat_norm(feat))
             return query + self.gamma.to(query.dtype) * attn
 
         if self.with_cp and query.requires_grad:
@@ -224,10 +259,14 @@ class InteractionBlock(nn.Module):
         extra_extractor=False,
         with_cp=False,
         normalize_interaction=False,
+        normalize_interaction_has_grad=False,
+        moe_cfg=None,
+        adapter_use_deform_attn=True,
     ):
         super().__init__()
 
         self.normalize_interaction = normalize_interaction
+        self.normalize_interaction_has_grad = normalize_interaction_has_grad
 
         self.injector = Injector(
             dim=dim,
@@ -238,6 +277,8 @@ class InteractionBlock(nn.Module):
             norm_layer=norm_layer,
             deform_ratio=deform_ratio,
             with_cp=with_cp,
+            moe_cfg=moe_cfg,
+            adapter_use_deform_attn=adapter_use_deform_attn,
         )
         self.extractor = Extractor(
             dim=dim,
@@ -251,6 +292,8 @@ class InteractionBlock(nn.Module):
             drop=drop,
             drop_path=drop_path,
             with_cp=with_cp,
+            moe_cfg=moe_cfg,
+            adapter_use_deform_attn=adapter_use_deform_attn,
         )
         if extra_extractor:
             self.extra_extractors = nn.Sequential(
@@ -266,6 +309,8 @@ class InteractionBlock(nn.Module):
                         drop=drop,
                         drop_path=drop_path,
                         with_cp=with_cp,
+                        moe_cfg=moe_cfg,
+                        adapter_use_deform_attn=adapter_use_deform_attn,
                     )
                     for _ in range(2)
                 ]
@@ -281,10 +326,15 @@ class InteractionBlock(nn.Module):
         x_pre = x[:, :-HW]
         x_patches = x[:, -HW:]
         if self.normalize_interaction:
-            with torch.no_grad():
+            if self.normalize_interaction_has_grad:
                 NORM = x_patches.norm(dim=-1, keepdim=True).mean(
                     dim=1, keepdim=True
                 ) / np.sqrt(x_patches.shape[-1])
+            else:
+                with torch.no_grad():
+                    NORM = x_patches.norm(dim=-1, keepdim=True).mean(
+                        dim=1, keepdim=True
+                    ) / np.sqrt(x_patches.shape[-1])
             x_patches = x_patches / NORM
         x_patches = self.injector(
             query=x_patches,
@@ -298,16 +348,20 @@ class InteractionBlock(nn.Module):
             x_patches = x_patches * NORM
         x = torch.cat((x_pre, x_patches), dim=1)
 
-        for idx, blk in enumerate(blocks):
+        for _, blk in enumerate(blocks):
             x = blk(x, H, W)
 
-        x_pre = x[:, :-HW]
         x_patches = x[:, -HW:]
         if self.normalize_interaction:
-            with torch.no_grad():
+            if self.normalize_interaction_has_grad:
                 NORM = x_patches.norm(dim=-1, keepdim=True).mean(
                     dim=1, keepdim=True
                 ) / np.sqrt(x_patches.shape[-1])
+            else:
+                with torch.no_grad():
+                    NORM = x_patches.norm(dim=-1, keepdim=True).mean(
+                        dim=1, keepdim=True
+                    ) / np.sqrt(x_patches.shape[-1])
             x_patches = x_patches / NORM
         c = self.extractor(
             query=c,
@@ -331,9 +385,7 @@ class InteractionBlock(nn.Module):
                     W=W,
                     **kwargs,
                 )
-        if self.normalize_interaction:
-            x_patches = x_patches * NORM
-        x = torch.cat((x_pre, x_patches), dim=1)
+
         return x, c
 
 
